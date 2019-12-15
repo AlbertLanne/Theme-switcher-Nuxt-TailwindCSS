0bba831f9469da99fe8d5f9240755b6dd6ca4e28
AlbertLanne <albert.lanne@gmail.com>
2019-12-15 18:46:54 +0100

    new informations add

--- components/NavBarre.vue
+++ components/NavBarre.vue
@@ -1,17 +1,15 @@
 <template>
   <header class=" bg-background-tertiary flex">
 <!--here i would like to add a button to switch the theme-->
+<div class="m-2 flex">
 
-    <div class=" mx-6 justify-start ">
+    <div class=" mx-6 justify-start mx-2 border-2 border-dotted rounded border-border-color-primary">
              <h1 class="m-3 my-6 text">❤ Website prototype ❤</h1>
-               <p> --nav barre--</p>
+
     </div>
+    <p> --nav barre--</p>
 
-<div class="flex justify-end items-center">
-<button class=" mx-2 border-2 rounded border-border-color-primary">
-  <p class="p-2">ButtonOne</p></button>
 </div>
-
   </header>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,36 @@
 
 
     <p>--index page--</p>
-      <p class="mt-6 text-center text-copy-primary">
+      <p class="mt-6 my-2 text-center text-copy-primary">
         <i>Prototype of global theme changer make in single js file</i>
       </p>
+
+    <table class="text-center items-center border-2 border-border-color-primary">
+      <thead>
+      <tr>
+        <th class="border border-border-color-primary px-4 py-2 text-copy-secondary">Step I did</th>
+        <th class="border border-border-color-primary px-4 py-2 text-copy-secondary">result</th>
+      </tr>
+      </thead>
+
+      <tr>
+        <td class="border border-border-color-primary px-4 py-2">Try to use the button on the same file of the methode</td>
+        <td class="border border-border-color-primary px-4 py-2">the button are under it's work well</td>
+      </tr>
+      <tr>
+        <td class="border border-border-color-primary px-4 py-2">try the button someone else</td>
+        <td class="border border-border-color-primary px-4 py-2">error eh don't know the methode</td>
+      </tr>
+      <tr>
+        <td class="border border-border-color-primary px-4 py-2">and try the button+ methode someone else</td>
+        <td class="border border-border-color-primary px-4 py-2">I have like a children of theme switcher (so 2 different)</td>
+      </tr>
+
+    </table>
+
+
+
+
     <p class="mt-6 text-center text-copy-secondary text-center">
         Vestibulum turpis sem, aliquet eget, lobortis pellentesque, rutrum eu, nisl. Pellentesque dapibus hendrerit tortor. Praesent venenatis metus at tortor pulvinar varius. Sed augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Ut non enim eleifend felis pretium feugiat. Maecenas vestibulum mollis diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sagittis. Praesent nec nisl a purus blandit viverra. Aenean massa. Pellentesque commodo eros a enim. Fusce fermentum. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Pellentesque commodo eros a enim.
     </p>
